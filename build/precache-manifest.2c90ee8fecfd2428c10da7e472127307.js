48aef609a7c3d26abccf00c43b8be0aa5adeaa43
Oleg <pavlicholeg@gmail.com>
2020-06-18 02:08:49 +0300

    еще

--- build/precache-manifest.2c90ee8fecfd2428c10da7e472127307.js
+++ build/precache-manifest.2c90ee8fecfd2428c10da7e472127307.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "c285261ad6bdf47930091d2a20655820",
+    "revision": "d75d5b079151b30efab3c3c140254674",
     "url": "https://pavlicholeg.github.io/dinabot_gscripts/build/index.html"
   },
   {
-    "revision": "0c4bb5cfd9109366723e",
+    "revision": "c120beac0b5cc9bfa259",
     "url": "https://pavlicholeg.github.io/dinabot_gscripts/build/static/css/main.5f361e03.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "https://pavlicholeg.github.io/dinabot_gscripts/build/static/js/2.6ad721cf.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "0c4bb5cfd9109366723e",
-    "url": "https://pavlicholeg.github.io/dinabot_gscripts/build/static/js/main.bf88a7e7.chunk.js"
+    "revision": "c120beac0b5cc9bfa259",
+    "url": "https://pavlicholeg.github.io/dinabot_gscripts/build/static/js/main.ee99b5a5.chunk.js"
   },
   {
     "revision": "5f1a6045866f37768755",
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://pavlicholeg.github.io/dinabot_gscripts/build/precache-manifest.ab2674931c93eaa682252a1c333ea65f.js"
+  "https://pavlicholeg.github.io/dinabot_gscripts/build/precache-manifest.2c90ee8fecfd2428c10da7e472127307.js"
 );
 
 self.addEventListener('message', (event) => {
